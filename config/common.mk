AOSiP/platform_vendor_aosip

[2/2] Add 'GoogleSans' font family
Change-Id: I15e7760d2b7b30cbfedc0dd3278ea20f924adc25
Signed-off-by: Jagrav Naik <jagravnaik0@gmail.com>

config/common.mk
@@ -110,3 +110,12 @@ include vendor/aosip/config/packages.mk
 
 # Versioning
 include vendor/aosip/config/version.mk
+
+# Fonts
+PRODUCT_COPY_FILES += \
+    vendor/aosip/fonts/GoogleSans-Regular.ttf:system/fonts/GoogleSans-Regular.ttf \
+    vendor/aosip/fonts/GoogleSans-Medium.ttf:system/fonts/GoogleSans-Medium.ttf \
+    vendor/aosip/fonts/GoogleSans-MediumItalic.ttf:system/fonts/GoogleSans-MediumItalic.ttf \
+    vendor/aosip/fonts/GoogleSans-Italic.ttf:system/fonts/GoogleSans-Italic.ttf \
+    vendor/aosip/fonts/GoogleSans-Bold.ttf:system/fonts/GoogleSans-Bold.ttf \
+    vendor/aosip/fonts/GoogleSans-BoldItalic.ttf:system/fonts/GoogleSans-BoldItalic.ttf

NucleaRom/android_vendor_lineage

Signed-off-by: acuicultor <ramoncuenca4@gmail.com>

config/common.mk
@@ -295,6 +295,15 @@ NR_DISPLAY_VERSION := $(NR_BUILD_VERSION)
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.nr.display.version=$(NR_DISPLAY_VERSION)
 
+# Fonts
+PRODUCT_COPY_FILES += \
+    vendor/nr/fonts/GoogleSans-Regular.ttf:system/fonts/GoogleSans-Regular.ttf \
+    vendor/nr/fonts/GoogleSans-Medium.ttf:system/fonts/GoogleSans-Medium.ttf \
+    vendor/nr/fonts/GoogleSans-MediumItalic.ttf:system/fonts/GoogleSans-MediumItalic.ttf \
+    vendor/nr/fonts/GoogleSans-Italic.ttf:system/fonts/GoogleSans-Italic.ttf \
+    vendor/nr/fonts/GoogleSans-Bold.ttf:system/fonts/GoogleSans-Bold.ttf \
+    vendor/nr/fonts/GoogleSans-BoldItalic.ttf:system/fonts/GoogleSans-BoldItalic.ttf 
+
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
 -include vendor/nr/config/partner_gms.mk
 -include vendor/cyngn/product.mk